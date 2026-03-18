e6442b0aa96d1ea95542f876a6f1d555b229e608
Csanad Beres <qed.csberes@gmail.com>
2026-03-18 03:23:40 +0100

    chore: harden eslint config
    ignore .worktrees/, add stricter rules (no-unused-vars, no-non-null-assertion, no-explicit-any, eqeqeq, no-var, curly)

--- eslint.config.js
+++ eslint.config.js
@@ -14,16 +14,26 @@ const noLineComments = {
 
 export default [
   {
+    ignores: ['.worktrees/**'],
+  },
+  {
     files: ['**/*.ts'],
     languageOptions: {
       parser: tseslint.parser,
     },
     plugins: {
+      '@typescript-eslint': tseslint.plugin,
       custom: { rules: { 'no-line-comments': noLineComments } },
     },
     rules: {
       'no-console': 'warn',
       'custom/no-line-comments': 'warn',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      'eqeqeq': 'error',
+      'no-var': 'error',
+      'curly': 'error',
     },
   },
 ];
